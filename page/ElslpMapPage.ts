90b6806435b51e3bbb49b5b0762019a520180970
happy <happy>
2019-09-15 21:34:43 +0800

    client批量提交

--- page/ElslpMapPage.ts
+++ page/ElslpMapPage.ts
@@ -102,6 +102,7 @@ module gameelslp.page {
 
             this._viewUI.mouseThrough = true;
             this._game.playMusic(Path_game_elslp.music_elslp + "toubao_bgm.mp3");
+            this._viewUI.btn_spread.left = this._game.isFullScreen ? 25 : 10;
         }
 
         // 页面打开时执行函数
